feb87eaac71626430ebfecb0ae2cb5af46efc289
Jakub Gladykowski <gladykov@gmail.com>
2023-11-13 01:16:40 -0300

    Clean go cache

--- clean-eos.sh
+++ clean-eos.sh
@@ -6,3 +6,11 @@ sudo pacman -Qtdq | sudo pacman -Rns -
 
 # Remove uninstalled packages in cache
 sudo paccache -ruk0
+
+# Clean go cache
+if type go &> /dev/null; then
+  echo "Cleaning go caches"
+  go clean -cache
+  go clean -fuzzcache
+  go clean -modcache
+fi
